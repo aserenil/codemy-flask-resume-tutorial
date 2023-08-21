19c44075b478195d7b317d3bb7716c062216266e
Hector Anthony Serenil <vercingetorix52@gmail.com>
2023-08-21 14:51:04 -0700

    first commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Create Index Page
+@app.route("/")
+def index():
+    first_name = "Bip"
+    favorite_pizza = ["Pepperoni", "Cheese", "Supreme", "Mushroom"]
+    return render_template("index.html", first_name=first_name, favorite_pizza=favorite_pizza)
+
+
+# Create About Page
+@app.route("/about")
+def about():
+    return render_template("about.html")
